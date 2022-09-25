080174b621f84904cb61cda2c3ccf75282330bde
Victor Toon <araujo.victor200@gmail.com>
2022-09-25 10:47:57 -0300

    added messages json

--- telegramBot/bot.py
+++ telegramBot/bot.py
@@ -1,12 +1,22 @@
 import asyncio
 import telegram
+import json
 
+with open('messages.json', 'r') as openfile:
+    # Reading from json file
+    json_object = json.load(openfile)
 
 async def main():
     bot = telegram.Bot("5654110178:AAE_L4yjLkhwqbSZlNzt-NSg7z5QOOBSJS8")
-    list = await bot.get_updates()
+    list = await bot.get_updates(offset=5)
     for element in list:
-        print(str(element.message['from']['id'])+':', element.message['text'])
+        if str(element.message['message_id']) not in json_object:
+            json_object[str(element.message['message_id'])] = element.message['text']
+            #print(str(element.message['from']['id'])+':', element.message['text'])
+            # Writing to sample.json
+            with open("messages.json", "w") as outfile:
+                outfile.write(json.dumps(json_object))
+            print(json_object)
 
 
 if __name__ == '__main__':
